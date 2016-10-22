39f62bfe195e3883de2eda99ca7aea4fc1cdbd06
tsartori <thiagosartori@live.com>
2016-10-22 15:56:40 -0200

    5

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
 <?php
 
-require_once 'bootstrap.php';
+define('__APP_ROOT__', __DIR__);
+
+require_once 'vendor/autoload.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PHP</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.min.css"
+        integrity="sha256-F7gqKszCwmz8vhiti+AICU8dLfIEpxzPVihhhGfbbKg=" crossorigin="anonymous" />
+    </head>
+    <body>
+        <nav class="nav has-shadow">
+            <div class="container">
+                <div class="nav-left ">
+                    <a class="nav-item" href="index.php">
+                        <img src="http://fagoc.br/download/a/logo" alt="logo" />
+                    </a>
+                </div>
+
+
+
+                <div class="nav-right nav-menu">
+                    <a class="nav-item" href="index.php?e=6">exercicio 6</a>
+                    <a class="nav-item" href="index.php?e=7">exercicio 7</a>
+                    <a class="nav-item" href="index.php?e=8">exercicio 8</a>
+                </div>
+            </div>
+        </nav>
+        <?php
+        use Fagoc\core\Router;
+
+        $router = new Router();
+        $router->get('/exercicio/6', function(){
+          $exercicio = __DIR__ . '/exercicio-6/' . 'file.php';
+          if (file_exists($exercicio)) {
+            require_once $exercicio;
+          }
+        });
+
+        ?>
+    </body>
+</html>
--- src/core/Router.php
+++ src/core/Router.php
@@ -5,6 +5,15 @@ class Router{
     private $method = 'get';
     private $uri='';
 
+    /**
+    *  routes[
+    *     'GET' => [
+    *      '/' => 'Faço isso',
+    *      '/exercicio-6/' =>
+    *
+    */
+    private $routes = [];
+
     public function __construct($uri = null, $method = null){
 
       if (!is_null($uri)) {
@@ -14,7 +23,26 @@ class Router{
       if (!is_null($method)) {
         $this->method = $method;
       }
+
     }
+
+    public function __call($name, $arguments){
+
+      $method = strtoupper($name);
+      if (!isset($this->routes[$method])) {
+        this->routes[$method] = [];
+      }
+
+      $route = arguments[0];
+      $callback = arguments[1];
+
+      this->routes[$method][$route] = $callback;
+
+      //var_dump($arguments);
+
+    }
+
+
   }
 
  ?>

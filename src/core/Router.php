fe2de9223fd6e3e5027d6545532fa6d727870e65
tsartori <thiagosartori@live.com>
2016-10-22 16:09:23 -0200

    2016/10/22 16:09

--- src/core/Router.php
+++ src/core/Router.php
@@ -1,58 +1,44 @@
 <?php
-namespace Fagoc/core;
-
-class Router{
+namespace Fagoc\Core;
+class Router
+{
     private $method = 'get';
-    private $uri='';
-
+    private $uri = '';
     /**
-    *  routes[
-    *     'GET' => [
-    *      '/' => 'Faço isso',
-    *      '/exercicio-6/' =>
-    *
+    * routes[
+    *  'GET' => [
+    *    '/' => 'faço isso',
+    *    '/exercicio/6' => 'carregar o arquivo do exercicio 6'
+    *    ]
+    * ]
     */
     private $routes = [];
-
-    public function __construct($uri = null, $method = null){
-
-      if (!is_null($uri)) {
-        $this->uri = $uri;
-      }
-
-      if (!is_null($method)) {
-        $this->method = $method;
-      }
-
-    }
-
-    public function __call($name, $arguments){
-
-      $method = strtoupper($name);
-      if (!isset($this->routes[$method])) {
-        this->routes[$method] = [];
-      }
-
-      $route = arguments[0];
-      $callback = arguments[1];
-
-      this->routes[$method][$route] = $callback;
-
-      //var_dump($arguments);
-
-    }
-
-    public function run(){
-
-      $routes = this->routes[this->method];
-      foreach (routes as $route => $callback) {
-        if ($route == this->uri) {
-          return call_user_func_array($callback,[]);
+    public function __construct($uri = null, $method = null)
+    {
+        if (!is_null($uri)) {
+            $this->uri = $uri;
         }
-      }
+        if (!is_null($method)) {
+            $this->method = $method;
+        }
     }
-
-
-  }
-
- ?>
+    public function __call($name, $arguments)
+    {
+        $method = strtoupper($name);
+        if (!isset($this->routes[$method])) {
+            $this->routes[$method] = [];
+        }
+        $route = $arguments[0];
+        $callback = $arguments[1];
+        $this->routes[$method][$route] = $callback;
+    }
+    public function run()
+    {
+        $routes = $this->routes[$this->method];
+        foreach($routes as $route => $callback) {
+            if ($route === $this->uri) {
+                return call_user_func_array($callback, []);
+            }
+        }
+    }
+}

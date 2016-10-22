cc393cce4daea4dd9605f5d45c02f28677659bec
tsartori <thiagosartori@live.com>
2016-10-22 15:16:09 -0200

    second

--- exercicio-6/file.php
+++ exercicio-6/file.php
@@ -8,7 +8,7 @@ require_once 'vendor/autoload.php';
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Meu Curso Legal</title>
+        <title>CURSO PHP</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.min.css"
         integrity="sha256-F7gqKszCwmz8vhiti+AICU8dLfIEpxzPVihhhGfbbKg=" crossorigin="anonymous" />
     </head>

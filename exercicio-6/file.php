d7f4a9f74db3e00fcb9ff35cef56587c4fc45598
tsartori <thiagosartori@live.com>
2016-10-22 15:13:27 -0200

    2016/10/22 first

--- exercicio-6/file.php
+++ exercicio-6/file.php
@@ -1,5 +1,37 @@
-<h2>Exercício 6</h2>
-<p>Explorar o uso de argumentos dinâmicos para as funções e organizar uma fluxo entre elas</p>
 <?php
-//function
-//require_once
+
+define('__APP_ROOT__', __DIR__);
+
+require_once 'vendor/autoload.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Meu Curso Legal</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.min.css"
+        integrity="sha256-F7gqKszCwmz8vhiti+AICU8dLfIEpxzPVihhhGfbbKg=" crossorigin="anonymous" />
+    </head>
+    <body>
+        <nav class="nav has-shadow">
+            <div class="container">
+                <div class="nav-left ">
+                    <a class="nav-item" href="index.php">
+                        <img src="http://fagoc.br/download/a/logo" alt="logo" />
+                    </a>
+                </div>
+                <div class="nav-right nav-menu">
+                    <a class="nav-item" href="index.php?e=6">exercicio 6</a>
+                    <a class="nav-item" href="index.php?e=7">exercicio 7</a>
+                    <a class="nav-item" href="index.php?e=8">exercicio 8</a>
+                </div>
+            </div>
+        </nav>
+        <?php
+        $exercicio = __DIR__ . '/exercicio-' . get('e') . '/' . 'file.php';
+        if (file_exists($exercicio)) {
+            require_once $exercicio;
+        }
+        ?>
+    </body>
+</html>

453689d42a9f9d7349a414cbc6add24af7248a3a
tsartori <thiagosartori@live.com>
2016-10-29 15:38:51 -0200

    teste

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -7,5 +7,18 @@ use Fagoc\Core\Input;
 class UsuarioController{
   public function salvar(Input $input){
     //var_dump($input);
+    ?>
+    <div class="section">
+      <div>
+        Registro salvo com sucesso!
+      </div>
+      <div>
+        <a class="button" href="<?php route ('exercicio/6'); ?>">
+          Voltar
+        </a>
+      </div>
+    </div>
+    <?php
+    
   }
 }
